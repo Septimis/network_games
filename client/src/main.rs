2d092af3f75e6f8375ca6e0df12e56d1a6715237
Septimis <connor@cmeads.com>
2024-12-04 22:30:35 +0000

    bare bones client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::env;
+
+fn main() -> std::io::Result<()> {
+	let args : Vec<String> = env::args().collect();
+
+	if args.len() != 4
+	{
+		panic!("Use: [ip_address] [port_number] [message]");
+	}
+
+	let ip_address : &str = &args[1];
+	let port_number : &str = &args[2];
+	let message : &str = &args[3];
+
+	let mut stream = TcpStream::connect("{ip_address}:{port_number}")?;
+
+	stream.write_all(message.as_bytes());
+
+	Ok(())
 }
