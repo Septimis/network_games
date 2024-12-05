4029dcbb9cbd81041f27271fdabc0e4cdf775f08
Septimis <connor@cmeads.com>
2024-12-05 15:54:46 +0000

    Server & client can now make a connection and talk

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,22 +1,43 @@
-use std::io::{Read, Write};
+use core::str;
+use std::io::{self, BufRead, Read, Write};
 use std::net::TcpStream;
 use std::env;
+use std::time::Duration;
 
-fn main() -> std::io::Result<()> {
+const BUFFER_SIZE : usize = 4096;
+
+fn main()
+{
 	let args : Vec<String> = env::args().collect();
 
-	if args.len() != 4
+	if args.len() != 3
 	{
-		panic!("Use: [ip_address] [port_number] [message]");
+		panic!("Invalid Args:\n\tUse: [Server IP Address] [Port Number]");
 	}
 
 	let ip_address : &str = &args[1];
 	let port_number : &str = &args[2];
-	let message : &str = &args[3];
 
-	let mut stream = TcpStream::connect("{ip_address}:{port_number}")?;
+	let server_address : String = format!("{}:{}", ip_address, port_number);
+	let mut stream : TcpStream = TcpStream::connect(&server_address).expect("Could not connect to server");
 
-	stream.write_all(message.as_bytes());
+	println!("Connected to the server at {}", server_address);
+	let mut reader : io::BufReader<TcpStream> = io::BufReader::new(stream.try_clone().unwrap());
+	let stdin : io::Stdin = io::stdin();
 
-	Ok(())
+	loop
+	{
+		let mut buffer : [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+		reader.read(&mut buffer).expect("Unable to read server message...");
+
+		println!("[Server] {}", str::from_utf8(&buffer).expect("Invalid UTF-8 String received..."));
+		buffer = [0; BUFFER_SIZE];
+
+		let mut input : String = String::new();
+		print!("Input: ");
+		stdin.read_line(&mut input).expect("Failed to read from stdin");
+
+		// Send the user's input to the server
+		stream.write_all(input.as_bytes()).expect("Failed to send data to server");
+	}
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,38 +1,199 @@
-use std::net::{TcpListener, TcpStream}; // Tcp functionality
-use std::env; // Reading command-line arguments
+use std::collections::HashMap;
+use std::io::{self, BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::env;
 
-fn handle_client(stream : TcpStream)
+#[derive(Debug)]
+struct GameRoom
 {
-	println!("New connection found at {}", stream.peer_addr().unwrap());
+	name: String,
+	game_type: String,
+	player1: Option<TcpStream>,
+	player2: Option<TcpStream>,
 }
 
-fn main() -> std::io::Result<()>
+#[derive(Clone)]
+struct Server
 {
-	let args : Vec<String> = env::args().collect();
+	rooms: Arc<Mutex<HashMap<String, GameRoom>>>,
+}
 
+impl Server
+{
+	fn new() -> Self
+	{
+		Server
+		{
+			rooms: Arc::new(Mutex::new(HashMap::new())),
+		}
+	}
+
+	fn handle_client(&self, mut stream : TcpStream)
+	{
+		log("Server", format!("Client connected\n\t{}", stream.peer_addr().unwrap().to_string()));
+
+		let mut reader : BufReader<TcpStream> = io::BufReader::new(stream.try_clone().unwrap());
+		let mut buffer = String::new();
+
+		loop
+		{
+			buffer.clear();
+			buffer.push_str("Choose from the following options:\n\t1) Create new room\n\t2) Join existing room\n\t3) Exit\n");
+			stream.write_all(buffer.as_bytes()).unwrap();
+			stream.flush().expect("msg");
+			log("Server", format!("{}", &buffer));
+
+			buffer.clear();
+			reader.read_line(&mut buffer).unwrap();
+			log("Client", format!("{}", &buffer));
+			match buffer.trim()
+			{
+				"1" => self.create_game_room(&mut stream).unwrap(),
+				"2" => self.join_game_room(&mut stream).unwrap(),
+				"3" => {
+					log("Server", "Ending connection".to_string());
+					break;
+				}
+				_ => {
+					stream.write_all(b"Invalid option. Please try again.\n").unwrap();
+					log("Server", format!("Invalid option. Please try again."));
+				}
+			}
+		}
+	}
+
+	fn create_game_room(&self, stream: &mut TcpStream) -> io::Result<()>
+	{
+		log("Server", "Starting the 'create game room' process...".to_string());
+
+		let mut reader : BufReader<TcpStream> = io::BufReader::new(stream.try_clone().unwrap());
+		let mut buffer : String = String::new();
+
+		stream.write_all(b"Enter game room name: ")?;
+		stream.flush()?;
+		log("Server", "Enter game room name: ".to_string());
+
+		reader.read_line(&mut buffer)?;
+		let room_name = buffer.trim().to_string();
+		buffer.clear();
+
+		let mut game_type : String = String::new();
+		while String::is_empty(&game_type)
+		{
+			stream.write_all(b"Choose game type (Tic Tac Toe / Connect 4): ")?;
+			stream.flush()?;
+			reader.read_line(&mut buffer)?;
+
+			game_type = match buffer.trim()
+			{
+				"1" => "Tic Tac Toe".to_string(),
+				"2" => "Connect 4".to_string(),
+				_ => {
+					stream.write_all(b"Invalid option... Please type the corresponding number...\n").unwrap();
+					"".to_string()
+				}
+			}
+		}
+
+		let mut rooms : std::sync::MutexGuard<'_, HashMap<String, GameRoom>> = self.rooms.lock().unwrap();
+		rooms.insert(
+			room_name.clone(),
+			GameRoom
+			{
+				name : room_name.clone(),
+				game_type,
+				player1 : Some(stream.try_clone().unwrap()),
+				player2 : None,
+			},
+		);
+
+		stream.write_all(b"Game room created. Waiting for a second player...\n")?;
+		// Wait for a second player to join
+		while let Some(room) = rooms.get_mut(&room_name)
+		{
+			if room.player2.is_none()
+			{
+				// Wait for another client to join
+				// This part can be improved with a more sophisticated approach
+				// For simplicity, we will just return here
+				return Ok(());
+			}
+		}
+
+		Ok(())
+	}
+
+	fn join_game_room(&self, stream: &mut TcpStream) -> io::Result<()>
+	{
+		let mut buffer : String = String::new();
+		let mut reader : BufReader<TcpStream> = io::BufReader::new(stream.try_clone().unwrap());
+
+		stream.write_all(b"Enter game room name: ")?;
+		stream.flush()?;
+		reader.read_line(&mut buffer)?;
+		let room_name : String = buffer.trim().to_string();
+
+		let mut rooms = self.rooms.lock().unwrap();
+		if let Some(room) = rooms.get_mut(&room_name)
+		{
+			if room.player2.is_none()
+			{
+				room.player2 = Some(stream.try_clone().unwrap());
+				stream.write_all(b"You have joined the game room.\n")?;
+				// Start the game logic here
+			}
+			else
+			{
+				stream.write_all(b"Game room is full.\n")?;
+			}
+		}
+		else
+		{
+			stream.write_all(b"Game room does not exist.\n")?;
+		}
+
+		Ok(())
+	}
+}
+
+fn main()
+{
+	let args : Vec<String> = std::env::args().collect();
 	if args.len() != 3
 	{
-		panic!("Use: [ip_address] [port number]");
+		panic!("Invalid arguments!\n\tUse: [Server IP address] [Port Number]");
 	}
+
 	let ip_address : &str = &args[1];
 	let port_number : &str = &args[2];
 
-	let listener = TcpListener::bind(ip_address)?;
+	let server : Server = Server::new();
+	let listener : TcpListener = TcpListener::bind(format!("{}:{}", ip_address, port_number)).unwrap();
+	println!("Server running on {}:{}", ip_address, port_number);
 
 	for stream in listener.incoming()
 	{
-		// Ensure a good connection before going to the 'handle_client' function
-		let mut stream = match stream
+		match stream
 		{
-			Ok(s) => s,
-			Err(e) => {
-				eprintln!("Failed to accept connection: {}", e);
-				continue;
+			Ok(stream) =>
+			{
+				let server : Server = server.clone();
+				thread::spawn(move ||
+				{
+					server.handle_client(stream);
+				});
 			}
-		};
-
-		handle_client(stream);
+			Err(e) =>
+			{
+				eprintln!("Error : {}", e);
+			}
+		}
 	}
+}
 
-	Ok(())
+fn log(owner : &str, log : String)
+{
+	println!("[{}] {}", owner, log);
 }
